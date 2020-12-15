3fd8f28d0a0908d351c378abe1227075f61cf79f
Magali Goncalves <magali.goncalvess@gmail.com>
2020-12-15 13:28:01 +0100

    image has been uploaded, but not displayed

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 const express = require("express");
 const app = express();
 const db = require('./db');
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
 
 app.use(express.static("public"));
+app.use(express.static("uploads"));
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    }
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
+
+app.use(uploader.single("image"));
 
 app.get('/images', (req, res) => {
     db.getImage().then(({ rows }) => {
-        console.log('rows', rows);
         res.json(rows);
     });
+});
+
+app.post("/upload", (req, res) => {
+    console.log("req.file", req.file);
+    console.log("req.file.path", req.file.path);
+    if(req.file) {
+        res.json({
+            success: true,
+            // img: `"${req.file.path}"`
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
 });
 
 app.listen(8080, () => console.log("Imageboard up and running :)"));
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,29 @@ new Vue({
     el: "#main",
     data: {
         images: [],
+        title: "",
+        img: null
+    },
+    methods: { 
+        handleFileChange: function(e) {
+            console.log("e.target.files", e.target.files);
+            //update the image property
+            this.image = e.target.files[0];
+        },
+        handleUpload: function(e) {
+            //prevent default behavior
+            e.preventDefault();
+            //POST data to the /uploads path with axios
+            var formData = new FormData();
+
+            formData.append("title", this.title);
+            formData.append("image", this.image);
+
+            axios.post("/upload", formData).then((res) => {
+                console.log("formData res", res);
+            });
+        }
+
     },
     mounted: function () {
         var self = this;
