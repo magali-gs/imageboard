423e6819508f4ec29b26f0278e9ca9fe205ec636
Magali Goncalves <magali.goncalvess@gmail.com>
2020-12-18 15:43:35 +0100

    part 5 almost completed

--- public/js/script.js
+++ public/js/script.js
@@ -15,14 +15,29 @@
             var self = this;
             axios
                 .get("/comments/" + this.imageId)
-                .then(function ({data}) {
-                    console.log(data);
+                .then(function ({ data }) {
                     self.comments = data;
                 })
                 .catch(function (error) {
                     console.log("error: ", error);
                 });
         },
+        watch: {
+            imageId: function () {
+                console.log("imageId prop just updated");
+                var self = this;
+                axios
+                    .get("/comments/" + this.imageId)
+                    .then(function ({ data }) {
+                        console.log(data);
+                        self.comments = data;
+                    })
+                    .catch(function (error) {
+                        console.log("error: ", error);
+                    });
+            },
+        },
+
         methods: {
             uploadComment: function (e) {
                 console.log("uploadComment");
@@ -34,10 +49,9 @@
                     userComment: this.userComment,
                 };
                 console.log(commentData);
-                axios.post("/comments", commentData)
-                    .then((res) => {
-                        self.comments.unshift(res.data);
-                    });
+                axios.post("/comments", commentData).then((res) => {
+                    self.comments.unshift(res.data);
+                });
             },
         },
     });
@@ -69,7 +83,26 @@
                 .catch(function (error) {
                     console.log("error: ", error);
                 });
-        },
+        },        
+        watch: {
+            imageId: function(){
+                console.log('imageId prop just updated');
+                var self = this;
+                axios
+                    .get("/highlighted/" + this.imageId)
+                    .then(function ({ data }) {
+                        self.title = data[0].title;
+                        self.description = data[0].description;
+                        self.username = data[0].username;
+                        self.created_at = data[0].created_at;
+                        self.url = data[0].url;
+                    })
+                    .catch(function (error) {
+                        console.log("error: ", error);
+                    });
+            }
+        }, 
+
         methods: {
             closeModal: function () {
                 console.log("closeModal click worked");
@@ -86,9 +119,7 @@
             description: "",
             username: "",
             image: null,
-            //change the value here to location.hash.slice(1)
-            // this value is set at the begin and we want it to change when it is clicked
-            imageId: null,
+            imageId: location.hash.slice(1),
             hasMore: true
         },
         mounted: function () {
@@ -101,23 +132,10 @@
                 .catch(function (error) {
                     console.log("error: ", error);
                 });
-            // //to change the value
-            // addEventListener('hashChange', function(){
-            //     console.log('Location change', location.hash);
-            // self.imageId = location.hash.slice(1)
-            // });
-            //remeber to change the value when the modal is closed
-            //the image is not chnage when the id is changed in the url
-            //use wathc...
+            addEventListener('hashchange', function(){
+                self.imageId = location.hash.slice(1);
+            });
         },
-        // watch: {
-        //     imageId: function(){
-        //         console.log('imageId prop just updated')
-        //         //change the imageId
-        //         //copy and past all the things in the mounted fucntion
-        //     }
-        // },
-
         methods: {
             handleFileChange: function (e) {
                 this.image = e.target.files[0];
@@ -134,14 +152,10 @@
                     this.images.unshift(res.data);
                 });
             },
-            // remove this method
-            getImageId: function (img) {
-                this.imageId = img.id;
-                console.log("getImageId worked");
-            },
 
             closeMe: function () {
                 console.log("closeme in the instance");
+                history.pushState({}, "", "/");
                 this.imageId = null;
             },
             moreImages: function () {
@@ -150,7 +164,6 @@
                 const lastId = this.images[this.images.length - 1].id;
                 axios.get("/more/" + lastId)
                     .then(({ data }) => {
-                        console.log(data);
                         data.forEach((element) => {
                             if(element.id == element.lowestId) {
                                 self.hasMore = false;
