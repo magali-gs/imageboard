36038314fcd2bb220f093f599b9d16e26294a063
Magali Goncalves <magali.goncalvess@gmail.com>
2020-12-20 22:08:20 +0100

    bug on comments fixed

--- sql/comments.sql
+++ sql/comments.sql
@@ -1,9 +1,16 @@
 DROP TABLE IF EXISTS comments;
 
-CREATE TABLE comments(
+CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL,
     comment TEXT,
     image_id INTEGER UNIQUE NOT NULL REFERENCES images(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+SELECT conname
+FROM pg_constraint
+WHERE conrelid = 'comments'::regclass
+  AND contype = 'u';
+
+  ALTER TABLE comments DROP CONSTRAINT comments_image_id_key;
